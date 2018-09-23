dff9891195afa8184b9a3845ed42ce9fc7d941b8
Joydeep Karmakar <joydeepkarmakar@hotmail.com>
2018-09-23 16:36:58 +0530

    SP Changes

--- ValidateUserAndMenu.sql
+++ ValidateUserAndMenu.sql
@@ -3,14 +3,15 @@
 EXEC [dbo].[ValidateUserAndMenu] 'jk.123test@gmail.com','P@ssw0rd'
 **/
 
-CREATE PROC [dbo].[ValidateUserAndMenu] @UserName AS VARCHAR(50),
-                                        @Password AS VARCHAR(50)
+ALTER PROC [dbo].[ValidateUserAndMenu] @UserName AS VARCHAR(50),
+                                       @Password AS VARCHAR(50)
 AS
      BEGIN
          DECLARE @ErrorMessage VARCHAR(200)= '';
          DECLARE @IsLoginValid BIT= 0;
          DECLARE @RoleName VARCHAR(50)= '';
-         DECLARE @UserFullName VARCHAR(250)= '';
+         DECLARE @UserFirstName VARCHAR(50)= '';
+         DECLARE @UserEmail VARCHAR(100)= '';
          IF NOT EXISTS
 (
     SELECT 1
@@ -42,13 +43,14 @@ AS
           AND [Password] = @Password
 )
              BEGIN
---SELECT * FROM dbo.MenuMaster
---SELECT * FROM dbo.RoleMaster
---SELECT * FROM dbo.UserAddressDetails
---SELECT * FROM dbo.UserProfileMaster
+		--SELECT * FROM dbo.MenuMaster
+		--SELECT * FROM dbo.RoleMaster
+		--SELECT * FROM dbo.UserAddressDetails
+		--SELECT * FROM dbo.UserProfileMaster
                  SELECT @RoleName = R.RoleName,
                         @IsLoginValid = 1,
-                        @UserFullName = U.User_Fname+ISNULL(U.User_Mname, '')+' '+U.User_Lname
+                        @UserFirstName = U.User_Fname,
+                        @UserEmail = U.Primary_Email
                  FROM dbo.[UserProfileMaster] U
                       INNER JOIN dbo.RoleMaster R ON U.RoleID = R.RoleID
                  WHERE U.Primary_Email = @UserName
@@ -56,5 +58,6 @@ AS
              END;
          SELECT @IsLoginValid AS IsLoginValid,
                 @RoleName AS RoleName,
-                @UserFullName AS UserFullName;
+                @UserFirstName AS UserFirstName,
+                @UserEmail AS UserEmail;
      END;
